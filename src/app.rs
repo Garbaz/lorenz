23a5c8b9fa83de585853785643729ce67fd413d6
Florian Hoffmann <florih@pm.me>
2023-06-09 20:40:33 +0200

    plot

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,29 @@
-use egui::Slider;
-use egui::plot::Plot;
+use egui::plot::{Line, Plot, PlotPoints};
+use egui::{Pos2, Slider};
 
 pub struct MyEguiApp {
     slider: f32,
+    points: Vec<Pos2>,
 }
 impl MyEguiApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        Self { slider: 0. }
+        Self {
+            slider: 0.,
+            points: vec![],
+        }
     }
 }
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.add(Slider::new(&mut self.slider, -10.0..=100.0));
-
-            ui.add(Plot::new(""));
+            let plot_points: PlotPoints = self
+                .points
+                .iter()
+                .map(|pos| [pos.x as f64, pos.y as f64])
+                .collect();
+            let line = Line::new(plot_points);
+            Plot::new("my_plot").show(ui, |pui| pui.line(line));
         });
-
     }
 }
